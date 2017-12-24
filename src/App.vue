4ab9fb7803f614d6d8eab0af7baaf20a97e22d39
Nenad Curcic <webfanatic82@gmail.com>
2017-12-24 20:36:06 +0100

    API request for venue search implemented, styling implemented, html implemented.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,161 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <div class="card" v-for="venue in venues">
+        <template v-for="specificVenue in allVenues">
+          <div class="card__photo-container">
+          </div>
+          <div class="card__info-container">
+            <div class="card__info-container__name-rating-container">
+              <h1>{{ venue.name }}</h1>
+              <div class="card__info-container__rating-container">
+                <p>{{ specificVenue.rating }}</p>
+              </div>
+            </div>
+            <p class="card__info-container__address">{{ venue.location.address }}, {{ venue.location.city }}</p>
+            <p class="card__info-container__coffee-cold warning"></p>
+          </div>
+        </template>
+      </div>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import axios from 'axios'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        venues: '',
+        place: '',
+        allVenues: ''
+      }
+    },
+    created() {
+      this.getPositionApproved()
+    },
+    methods: {
+      getPosition() {
+        return new Promise(function (resolve, reject) {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        })
+      },
+      getPositionApproved() {
+        this.getPosition()
+          .then((position) => {
+            let lat = position.coords.latitude,
+              lng = position.coords.longitude
+              axios.get('https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d1e0931735&v=20131016&ll=' + lat + ',' + lng + '&radius=1000&client_id=O4N5MBHQWS11LRWBBO15JTZWFC42WKSUKTQYMXJ1ZN1CIPXD&client_secret=X1043GY3LH0W4S54GT0RWL300R2144W5WUVJKQ30GI0O1F03&v=20120609')
+                .then(res => {
+                  this.venues = res.data.response.venues
+
+                  this.place = this.venues.map(function(place) {
+
+                    let venueID = place.id
+
+                    axios.get('https://api.foursquare.com/v2/venues/' + venueID + '/?client_id=O4N5MBHQWS11LRWBBO15JTZWFC42WKSUKTQYMXJ1ZN1CIPXD&client_secret=X1043GY3LH0W4S54GT0RWL300R2144W5WUVJKQ30GI0O1F03&v=20120609')
+                      .then( data => {
+                        console.log(data)
+                        this.allVenues = data.data.response.venue.rating
+                        console.log(rating)
+                      })
+                  })
+
+                })
+
+          })
+      }
     }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  .card {
+    display: flex;
+    width: 30%;
+    background-color: #efeff4;
+    border-top: 1px solid #c7cdcf;
+    margin: 10px auto;
+    padding: 20px 20px 10px;
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    &__photo-container {
+      display: flex;
+      width: 200px;
+      height: 130px;
+      margin-right: 20px;
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+      img {
+        width: 100%;
+      }
+    }
 
-a {
-  color: #42b983;
-}
+    &__info-container {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+
+      &__name-rating-container {
+        display: flex;
+        justify-content: space-between;
+
+        h1 {
+          font-size: 35px;
+          color: #2d5be3;
+          vertical-align: top;
+          margin: 0 10px 0 0;
+        }
+      }
+
+      &__rating-container {
+        display: flex;
+        justify-content: center;
+        width: 40px;
+        height: 40px;
+        background-color: green;
+        border-radius: 50%;
+
+        p {
+          margin-top: 0px;
+          margin-bottom: 0px;
+          align-self: center;
+          color: #fff;
+          font-size: 20px;
+        }
+      }
+
+      &__address {
+        font-size: 20px;
+        font-weight: bold;
+        text-transform: capitalize;
+        margin: 10px 0;
+      }
+
+      &__coffee-cold {
+        margin: 5px 0 10px 0;
+      }
+    }
+
+    .success {
+      color: green;
+    }
+
+    .warning {
+      color: red;
+    }
+  }
+
+  .error {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 50px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 import App from './App.vue'
-
-Vue.use(VueAxios, axios)
 
 new Vue({
   el: '#app',
